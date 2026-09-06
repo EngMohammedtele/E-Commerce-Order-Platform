73064d84c23af443afc054862e843ef876196c30
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 14:28:29 +0400

    docs: label cart item comments

--- src/main/java/com/codelegends/ecommerce/entity/CartItem.java
+++ src/main/java/com/codelegends/ecommerce/entity/CartItem.java
@@ -8,11 +8,15 @@ import lombok.*;
 @Table(uniqueConstraints=@UniqueConstraint(columnNames= {
     "cart_id","product_id"
 }
+// Defines the CartItem type used by the ecommerce app
 )) public class CartItem extends BaseClass {
     @Column(nullable=false)
+    // Store the quantity value for this object
     private Integer quantity;
     @ManyToOne(optional=false,fetch=FetchType.LAZY)
+    // Store the cart value for this object
     private Cart cart;
     @ManyToOne(optional=false,fetch=FetchType.LAZY)
+    // Store the product value for this object
     private Product product;
 }
