8a49bd2d4a4ae14730d99439bd7b8a8932d7302e
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 15:04:03 +0400

    docs: document category repository role

--- src/main/java/com/codelegends/ecommerce/repository/CategoryRepository.java
+++ src/main/java/com/codelegends/ecommerce/repository/CategoryRepository.java
@@ -3,6 +3,7 @@ import com.codelegends.ecommerce.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.*;
 // Describes the CategoryRepository contract used by this layer
+// Stores query methods for Category entities
 public interface CategoryRepository extends JpaRepository<Category,Long> {
     // Fetch only records that are still active
     List<Category> findAllByIsActiveTrue();
