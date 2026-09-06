174d8a09d5b7ca1c8c8a8ff74cd9937e2f950ea8
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 14:28:30 +0400

    comment: explain product comments

--- src/main/java/com/codelegends/ecommerce/entity/Product.java
+++ src/main/java/com/codelegends/ecommerce/entity/Product.java
@@ -8,23 +8,33 @@ import java.util.*;
 @Setter
 @NoArgsConstructor
 @Table(uniqueConstraints=@UniqueConstraint(columnNames="sku"))
+// Defines the Product type used by the ecommerce app
 public class Product extends BaseClass {
     @Column(nullable=false,length=120)
+    // Store the name value for this object
     private String name;
     @Column(nullable=false,precision=12,scale=2)
+    // Store the price value for this object
     private BigDecimal price;
     @Column(nullable=false)
+    // Store the stockQuantity value for this object
     private Integer stockQuantity;
     @Column(nullable=false,length=50)
+    // Store the sku value for this object
     private String sku;
     @ManyToOne(optional=false,fetch=FetchType.LAZY)
+    // Store the category value for this object
     private Category category;
     @ManyToOne(optional=false,fetch=FetchType.LAZY)
+    // Store the store value for this object
     private Store store;
     @OneToMany(mappedBy="product")
+    // Store the list of related cartItems entries
     private List<CartItem> cartItems=new ArrayList<>();
     @OneToMany(mappedBy="product")
+    // Store the list of related orderItems entries
     private List<OrderItem> orderItems=new ArrayList<>();
     @OneToMany(mappedBy="product")
+    // Store the list of related reviews entries
     private List<Review> reviews=new ArrayList<>();
 }
