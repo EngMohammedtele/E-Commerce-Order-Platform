f484006d4e3f0f94b36ef8a6b2755d9c0c0b1800
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 14:28:32 +0400

    docs: label order item repository comments

--- src/main/java/com/codelegends/ecommerce/repository/OrderItemRepository.java
+++ src/main/java/com/codelegends/ecommerce/repository/OrderItemRepository.java
@@ -2,8 +2,11 @@ package com.codelegends.ecommerce.repository;
 import com.codelegends.ecommerce.entity.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.*;
+// Describes the OrderItemRepository contract used by this layer
 public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {
+    // Fetch only records that are still active
     List<OrderItem> findAllByIsActiveTrue();
+    // Look up an active record by its id
     Optional<OrderItem> findByIdAndIsActiveTrue(Long id);
     boolean existsByOrderCustomerIdAndProductIdAndOrderIsActiveTrue(Long c,Long p);
 }
