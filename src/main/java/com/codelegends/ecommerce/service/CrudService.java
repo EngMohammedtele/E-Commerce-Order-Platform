1514ec1d61a319ff29bf08df78248466ca029301
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 14:28:33 +0400

    docs: explain crud service comments

--- src/main/java/com/codelegends/ecommerce/service/CrudService.java
+++ src/main/java/com/codelegends/ecommerce/service/CrudService.java
@@ -1,9 +1,15 @@
 package com.codelegends.ecommerce.service;
 import java.util.List;
+// Describes the CrudService contract used by this layer
 public interface CrudService<D> {
+    // Create a new record from the supplied data
     D create(D dto);
+    // Return all active records for this resource
     List<D> getAll();
+    // Find one record using the requested id
     D getById(Long id);
+    // Update the stored record with new values
     D update(Long id,D dto);
+    // Mark the selected record as inactive
     void delete(Long id);
 }
