319b501ffc118089b7efbcb68821fb3dffc46b7f
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 14:28:28 +0400

    docs: document ecommerce application comments

--- src/main/java/com/codelegends/ecommerce/ECommerceApplication.java
+++ src/main/java/com/codelegends/ecommerce/ECommerceApplication.java
@@ -2,8 +2,10 @@ package com.codelegends.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
+// Defines the ECommerceApplication type used by the ecommerce app
 public class ECommerceApplication {
     public static void main(String[] args) {
+        // Start the Spring Boot application
         SpringApplication.run(ECommerceApplication.class, args);
     }
 }
