72a4add63f4d069570f215679e9460c0c0196c59
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 15:04:03 +0400

    comment: explain cart item repository role

--- src/main/java/com/codelegends/ecommerce/repository/CartItemRepository.java
+++ src/main/java/com/codelegends/ecommerce/repository/CartItemRepository.java
@@ -3,6 +3,7 @@ import com.codelegends.ecommerce.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.*;
 // Describes the CartItemRepository contract used by this layer
+// Provides database access for cart item rows
 public interface CartItemRepository extends JpaRepository<CartItem,Long> {
     // Fetch only records that are still active
     List<CartItem> findAllByIsActiveTrue();
