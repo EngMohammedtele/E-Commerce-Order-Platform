719a54b4320405a2700cc6099b7b453f75439d4e
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 14:28:31 +0400

    comments: outline customer repository comments

--- src/main/java/com/codelegends/ecommerce/repository/CustomerRepository.java
+++ src/main/java/com/codelegends/ecommerce/repository/CustomerRepository.java
@@ -2,7 +2,10 @@ package com.codelegends.ecommerce.repository;
 import com.codelegends.ecommerce.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.*;
+// Describes the CustomerRepository contract used by this layer
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
+    // Fetch only records that are still active
     List<Customer> findAllByIsActiveTrue();
+    // Look up an active record by its id
     Optional<Customer> findByIdAndIsActiveTrue(Long id);
 }
