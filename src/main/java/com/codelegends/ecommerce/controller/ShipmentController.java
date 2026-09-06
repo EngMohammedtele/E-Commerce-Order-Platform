0afae664a13441fdda851ad2732d9b99468b6971
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 14:28:25 +0400

    docs: label shipment controller comments

--- src/main/java/com/codelegends/ecommerce/controller/ShipmentController.java
+++ src/main/java/com/codelegends/ecommerce/controller/ShipmentController.java
@@ -4,6 +4,7 @@ import com.codelegends.ecommerce.service.ShipmentService;
 import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/shipments")
+// Handles web requests for shipment operations
 public class ShipmentController extends AbstractCrudController<ShipmentDTO> {
     public ShipmentController(ShipmentService s) {
         super(s);
