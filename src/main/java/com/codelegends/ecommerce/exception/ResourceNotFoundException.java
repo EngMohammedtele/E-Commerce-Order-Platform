6e36fc82aba131358a07225989c3824df849adcf
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 14:56:46 +0400

    comment: explain not found inheritance

--- src/main/java/com/codelegends/ecommerce/exception/ResourceNotFoundException.java
+++ src/main/java/com/codelegends/ecommerce/exception/ResourceNotFoundException.java
@@ -1,6 +1,7 @@
 package com.codelegends.ecommerce.exception;
 // Represents an error case that the API can report clearly
 // Represents an error when requested data is missing
+// Use RuntimeException so services can throw it directly
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
