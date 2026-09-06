3ccf232877760392f10ae389474bdf98412f7f2f
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 14:28:29 +0400

    comment: describe category comments

--- src/main/java/com/codelegends/ecommerce/entity/Category.java
+++ src/main/java/com/codelegends/ecommerce/entity/Category.java
@@ -6,13 +6,18 @@ import java.util.*;
 @Getter
 @Setter
 @NoArgsConstructor
+// Defines the Category type used by the ecommerce app
 public class Category extends BaseClass {
     @Column(nullable=false,length=100)
+    // Store the name value for this object
     private String name;
     @Column(length=500)
+    // Store the description value for this object
     private String description;
     @ManyToOne(optional=false,fetch=FetchType.LAZY)
+    // Store the store value for this object
     private Store store;
     @OneToMany(mappedBy="category")
+    // Store the list of related products entries
     private List<Product> products=new ArrayList<>();
 }
