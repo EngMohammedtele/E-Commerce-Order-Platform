9c394008e0557178c72fa6b42cae01fbcc08f415
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 14:47:37 +0400

    comments: clarify cart customer join

--- src/main/java/com/codelegends/ecommerce/entity/Cart.java
+++ src/main/java/com/codelegends/ecommerce/entity/Cart.java
@@ -8,9 +8,12 @@ import java.util.*;
 @Getter
 // Let Lombok create setter methods for fields
 @Setter
+// Give JPA an empty constructor to create objects
 @NoArgsConstructor
 // Defines the Cart type used by the ecommerce app
+// Represents the Cart database object in Java
 public class Cart extends BaseClass {
+    // Connect this Cart to one customer record
     @OneToOne(optional=false,fetch=FetchType.LAZY)
     @JoinColumn(unique=true)
     // Store the customer value for this object
