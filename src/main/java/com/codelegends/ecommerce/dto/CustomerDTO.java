496e8b23eab896eb41d814b191e0cada3f78192a
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 14:28:27 +0400

    docs: note customer DTO comments

--- src/main/java/com/codelegends/ecommerce/dto/CustomerDTO.java
+++ src/main/java/com/codelegends/ecommerce/dto/CustomerDTO.java
@@ -7,27 +7,38 @@ import java.util.*;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+// Moves customer data between the API and service layers
 public class CustomerDTO {
+    // Store the id value for this object
     private Long id;
     @NotBlank
     @Size(max=100)
+    // Store the name value for this object
     private String name;
     @NotBlank
     @Email
     @Size(max=150)
+    // Store the email value for this object
     private String email;
     @NotBlank
     @Size(max=20)
+    // Store the phoneNumber value for this object
     private String phoneNumber;
     @NotNull
+    // Store the gender value for this object
     private Enums.Gender gender;
     @NotNull
     @Positive
+    // Store the storeId value for this object
     private Long storeId;
+    // Convert one entity object into a DTO
     public static CustomerDTO convertToDTO(Customer e) {
+        // Build and return the DTO with copied values
         return builder().id(e.getId()).name(e.getName()).email(e.getEmail()).phoneNumber(e.getPhoneNumber()).gender(e.getGender()).storeId(e.getStore().getId()).build();
     }
+    // Convert each entity in the list into a DTO
     public static List<CustomerDTO> convertToDTO(List<Customer>x) {
+        // Return the converted stream results to the caller
         return x.stream().map(CustomerDTO::convertToDTO).toList();
     }
 }
