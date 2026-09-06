456c1b1bdf56b2edf6f9113c21d57300e477fafa
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 14:28:28 +0400

    comment: explain address comments

--- src/main/java/com/codelegends/ecommerce/entity/Address.java
+++ src/main/java/com/codelegends/ecommerce/entity/Address.java
@@ -6,16 +6,22 @@ import static com.codelegends.ecommerce.entity.Enums.*;
 @Getter
 @Setter
 @NoArgsConstructor
+// Defines the Address type used by the ecommerce app
 public class Address extends BaseClass {
     @Column(nullable=false,length=200)
+    // Store the street value for this object
     private String street;
     @Column(nullable=false,length=100)
+    // Store the city value for this object
     private String city;
     @Column(nullable=false,length=20)
+    // Store the postalCode value for this object
     private String postalCode;
     @Enumerated(EnumType.STRING)
     @Column(nullable=false,length=10)
+    // Store the type value for this object
     private AddressType type;
     @ManyToOne(optional=false,fetch=FetchType.LAZY)
+    // Store the customer value for this object
     private Customer customer;
 }
