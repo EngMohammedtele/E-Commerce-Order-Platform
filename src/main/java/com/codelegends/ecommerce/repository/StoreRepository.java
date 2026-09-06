34f3bede0f790ab52b5e76444e2a0f68cacf9da8
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 14:28:33 +0400

    comment: explain store repository comments

--- src/main/java/com/codelegends/ecommerce/repository/StoreRepository.java
+++ src/main/java/com/codelegends/ecommerce/repository/StoreRepository.java
@@ -2,7 +2,10 @@ package com.codelegends.ecommerce.repository;
 import com.codelegends.ecommerce.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.*;
+// Describes the StoreRepository contract used by this layer
 public interface StoreRepository extends JpaRepository<Store,Long> {
+    // Fetch only records that are still active
     List<Store> findAllByIsActiveTrue();
+    // Look up an active record by its id
     Optional<Store> findByIdAndIsActiveTrue(Long id);
 }
