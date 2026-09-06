8bbac706da7879fd568b509cb128989a24e7c386
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 14:47:44 +0400

    comments: clarify product price and stock

--- src/main/java/com/codelegends/ecommerce/entity/Product.java
+++ src/main/java/com/codelegends/ecommerce/entity/Product.java
@@ -16,14 +16,20 @@ import java.util.*;
 // Defines the Product type used by the ecommerce app
 // Represents the Product database object in Java
 public class Product extends BaseClass {
+    // Require the name column to have a value
     @Column(nullable=false,length=120)
     // Store the name value for this object
+    // Store the Product name in the database
     private String name;
+    // Require the price column to have a value
     @Column(nullable=false,precision=12,scale=2)
     // Store the price value for this object
+    // Store the product selling price
     private BigDecimal price;
+    // Require the stock quantity column to have a value
     @Column(nullable=false)
     // Store the stockQuantity value for this object
+    // Track how many items are in stock
     private Integer stockQuantity;
     @Column(nullable=false,length=50)
     // Store the sku value for this object
