780f3394d06bf1d1ad7ffc485ca3ebed4f2bae24
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 14:28:31 +0400

    comments: describe address repository comments

--- src/main/java/com/codelegends/ecommerce/repository/AddressRepository.java
+++ src/main/java/com/codelegends/ecommerce/repository/AddressRepository.java
@@ -2,7 +2,10 @@ package com.codelegends.ecommerce.repository;
 import com.codelegends.ecommerce.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.*;
+// Describes the AddressRepository contract used by this layer
 public interface AddressRepository extends JpaRepository<Address,Long> {
+    // Fetch only records that are still active
     List<Address> findAllByIsActiveTrue();
+    // Look up an active record by its id
     Optional<Address> findByIdAndIsActiveTrue(Long id);
 }
