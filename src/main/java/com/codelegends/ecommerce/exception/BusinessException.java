5d7c16ce90935d4c9d52822eac9b6a0dc2135429
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 14:55:47 +0400

    docs: describe business exception role

--- src/main/java/com/codelegends/ecommerce/exception/BusinessException.java
+++ src/main/java/com/codelegends/ecommerce/exception/BusinessException.java
@@ -1,5 +1,6 @@
 package com.codelegends.ecommerce.exception;
 // Represents an error case that the API can report clearly
+// Represents a business rule problem in the app
 public class BusinessException extends RuntimeException {
     public BusinessException(String message) {
         super(message);
