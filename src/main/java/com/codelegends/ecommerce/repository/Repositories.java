76b34ee454065aca4023f3fbf1679917d59147d1
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 15:04:07 +0400

    comments: describe repository marker class

--- src/main/java/com/codelegends/ecommerce/repository/Repositories.java
+++ src/main/java/com/codelegends/ecommerce/repository/Repositories.java
@@ -1,6 +1,7 @@
 package com.codelegends.ecommerce.repository;
 // Marker class; repository interfaces are kept in separate files for Spring Data discovery.
 // Defines the Repositories type used by the ecommerce app
+// Groups repository package documentation in one marker type
 public final class Repositories {
     private Repositories() {
     }
