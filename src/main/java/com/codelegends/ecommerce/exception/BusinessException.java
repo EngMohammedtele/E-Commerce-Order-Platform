aad826da50fb57e26c56b9041511246fce573128
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 14:28:30 +0400

    comment: describe business exception comments

--- src/main/java/com/codelegends/ecommerce/exception/BusinessException.java
+++ src/main/java/com/codelegends/ecommerce/exception/BusinessException.java
@@ -1,4 +1,5 @@
 package com.codelegends.ecommerce.exception;
+// Represents an error case that the API can report clearly
 public class BusinessException extends RuntimeException {
     public BusinessException(String message) {
         super(message);
