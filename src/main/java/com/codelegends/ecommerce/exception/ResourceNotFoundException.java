c0443fdcee148d2b78ec3e1dec79a950d64b936b
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 14:56:46 +0400

    docs: document not found constructor

--- src/main/java/com/codelegends/ecommerce/exception/ResourceNotFoundException.java
+++ src/main/java/com/codelegends/ecommerce/exception/ResourceNotFoundException.java
@@ -3,6 +3,7 @@ package com.codelegends.ecommerce.exception;
 // Represents an error when requested data is missing
 // Use RuntimeException so services can throw it directly
 public class ResourceNotFoundException extends RuntimeException {
+    // Create the exception with a clear not-found message
     public ResourceNotFoundException(String message) {
         super(message);
     }
