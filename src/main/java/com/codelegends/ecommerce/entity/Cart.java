9dea03caaf58a464f1ff976f1b68676d2de66dfb
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 14:28:28 +0400

    comments: outline cart comments

--- src/main/java/com/codelegends/ecommerce/entity/Cart.java
+++ src/main/java/com/codelegends/ecommerce/entity/Cart.java
@@ -6,10 +6,13 @@ import java.util.*;
 @Getter
 @Setter
 @NoArgsConstructor
+// Defines the Cart type used by the ecommerce app
 public class Cart extends BaseClass {
     @OneToOne(optional=false,fetch=FetchType.LAZY)
     @JoinColumn(unique=true)
+    // Store the customer value for this object
     private Customer customer;
     @OneToMany(mappedBy="cart",cascade=CascadeType.ALL,orphanRemoval=true)
+    // Store the list of related items entries
     private List<CartItem> items=new ArrayList<>();
 }
