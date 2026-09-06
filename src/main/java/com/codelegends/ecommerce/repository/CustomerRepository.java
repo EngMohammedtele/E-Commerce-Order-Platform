968c7371b8f92133de86d75c7fe3d277df13ec78
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 15:04:04 +0400

    comments: clarify customer repository inheritance

--- src/main/java/com/codelegends/ecommerce/repository/CustomerRepository.java
+++ src/main/java/com/codelegends/ecommerce/repository/CustomerRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.*;
 // Describes the CustomerRepository contract used by this layer
 // Provides database operations for Customer entities
+// Inherit standard save, find, and delete methods from JpaRepository
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
     // Fetch only records that are still active
     List<Customer> findAllByIsActiveTrue();
