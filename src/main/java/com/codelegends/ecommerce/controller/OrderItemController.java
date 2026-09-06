1eb267d3f9013169a6eb17c2fe63133ee197add5
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 14:28:25 +0400

    docs: clarify order item controller comments

--- src/main/java/com/codelegends/ecommerce/controller/OrderItemController.java
+++ src/main/java/com/codelegends/ecommerce/controller/OrderItemController.java
@@ -4,6 +4,7 @@ import com.codelegends.ecommerce.service.OrderItemService;
 import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/order-items")
+// Handles web requests for orderitem operations
 public class OrderItemController extends AbstractCrudController<OrderItemDTO> {
     public OrderItemController(OrderItemService s) {
         super(s);
