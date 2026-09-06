f33268e6c35f945a8f50e8baee1b46ab459a61da
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 14:47:43 +0400

    docs: describe payment entity annotations

--- src/main/java/com/codelegends/ecommerce/entity/Payment.java
+++ src/main/java/com/codelegends/ecommerce/entity/Payment.java
@@ -4,9 +4,13 @@ import lombok.*;
 import java.time.*;
 import java.math.*;
 import static com.codelegends.ecommerce.entity.Enums.*;
+// Mark Payment as a table-backed database entity
 @Entity
+// Let Lombok create getter methods for fields
 @Getter
+// Let Lombok create setter methods for fields
 @Setter
+// Give JPA an empty constructor to create objects
 @NoArgsConstructor
 // Defines the Payment type used by the ecommerce app
 public class Payment extends BaseClass {
