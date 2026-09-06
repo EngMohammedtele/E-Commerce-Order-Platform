b0538cfa9685de4aa943f34bc07ea91d84443e77
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 14:28:30 +0400

    docs: label store comments

--- src/main/java/com/codelegends/ecommerce/entity/Store.java
+++ src/main/java/com/codelegends/ecommerce/entity/Store.java
@@ -6,15 +6,21 @@ import java.util.*;
 @Getter
 @Setter
 @NoArgsConstructor
+// Defines the Store type used by the ecommerce app
 public class Store extends BaseClass {
     @Column(nullable=false,length=100)
+    // Store the name value for this object
     private String name;
     @Column(nullable=false,length=200)
+    // Store the location value for this object
     private String location;
     @OneToMany(mappedBy="store")
+    // Store the list of related categories entries
     private List<Category> categories=new ArrayList<>();
     @OneToMany(mappedBy="store")
+    // Store the list of related products entries
     private List<Product> products=new ArrayList<>();
     @OneToMany(mappedBy="store")
+    // Store the list of related customers entries
     private List<Customer> customers=new ArrayList<>();
 }
