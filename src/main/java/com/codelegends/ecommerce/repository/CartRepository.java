148f3617073ed5eb5d535760f6e9853c913cfe19
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 14:28:31 +0400

    comment: explain cart repository comments

--- src/main/java/com/codelegends/ecommerce/repository/CartRepository.java
+++ src/main/java/com/codelegends/ecommerce/repository/CartRepository.java
@@ -2,8 +2,12 @@ package com.codelegends.ecommerce.repository;
 import com.codelegends.ecommerce.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.*;
+// Describes the CartRepository contract used by this layer
 public interface CartRepository extends JpaRepository<Cart,Long> {
+    // Fetch only records that are still active
     List<Cart> findAllByIsActiveTrue();
+    // Look up an active record by its id
     Optional<Cart> findByIdAndIsActiveTrue(Long id);
+    // Search active records using this field value
     Optional<Cart> findByCustomerIdAndIsActiveTrue(Long id);
 }
