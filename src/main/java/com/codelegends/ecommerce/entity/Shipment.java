2163956b1e6603b4657204744a2213bf7dee4d83
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 14:28:30 +0400

    comments: outline shipment comments

--- src/main/java/com/codelegends/ecommerce/entity/Shipment.java
+++ src/main/java/com/codelegends/ecommerce/entity/Shipment.java
@@ -8,14 +8,19 @@ import static com.codelegends.ecommerce.entity.Enums.*;
 @Setter
 @NoArgsConstructor
 @Table(uniqueConstraints=@UniqueConstraint(columnNames="trackingNumber"))
+// Defines the Shipment type used by the ecommerce app
 public class Shipment extends BaseClass {
     @Column(nullable=false,length=80)
+    // Store the trackingNumber value for this object
     private String trackingNumber;
     @Enumerated(EnumType.STRING)
     @Column(nullable=false,length=20)
+    // Store the status value for this object
     private ShipmentStatus status;
+    // Store the shippedDate value for this object
     private LocalDateTime shippedDate;
     @OneToOne(optional=false,fetch=FetchType.LAZY)
     @JoinColumn(unique=true)
+    // Store the order value for this object
     private Order order;
 }
