51f40698e541d10923683937ab7df03d2b8954b5
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 14:28:31 +0400

    docs: document cart item repository comments

--- src/main/java/com/codelegends/ecommerce/repository/CartItemRepository.java
+++ src/main/java/com/codelegends/ecommerce/repository/CartItemRepository.java
@@ -2,8 +2,12 @@ package com.codelegends.ecommerce.repository;
 import com.codelegends.ecommerce.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.*;
+// Describes the CartItemRepository contract used by this layer
 public interface CartItemRepository extends JpaRepository<CartItem,Long> {
+    // Fetch only records that are still active
     List<CartItem> findAllByIsActiveTrue();
+    // Look up an active record by its id
     Optional<CartItem> findByIdAndIsActiveTrue(Long id);
+    // Search active records using this field value
     Optional<CartItem> findByCartIdAndProductIdAndIsActiveTrue(Long c,Long p);
 }
