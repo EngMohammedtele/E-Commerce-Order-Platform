00b95dc84bf30be624a5111fb650e36d496373c7
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 14:28:32 +0400

    docs: document shipment repository comments

--- src/main/java/com/codelegends/ecommerce/repository/ShipmentRepository.java
+++ src/main/java/com/codelegends/ecommerce/repository/ShipmentRepository.java
@@ -2,7 +2,10 @@ package com.codelegends.ecommerce.repository;
 import com.codelegends.ecommerce.entity.Shipment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.*;
+// Describes the ShipmentRepository contract used by this layer
 public interface ShipmentRepository extends JpaRepository<Shipment,Long> {
+    // Fetch only records that are still active
     List<Shipment> findAllByIsActiveTrue();
+    // Look up an active record by its id
     Optional<Shipment> findByIdAndIsActiveTrue(Long id);
 }
