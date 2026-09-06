2bfbe55d46cb3b5a8fe3c36a3283487f000e3617
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 14:56:46 +0400

    docs: describe resource not found exception

--- src/main/java/com/codelegends/ecommerce/exception/ResourceNotFoundException.java
+++ src/main/java/com/codelegends/ecommerce/exception/ResourceNotFoundException.java
@@ -1,5 +1,6 @@
 package com.codelegends.ecommerce.exception;
 // Represents an error case that the API can report clearly
+// Represents an error when requested data is missing
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
