0af406af269b2808f05a6c88e32cceb71ba2f35a
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 14:28:35 +0400

    comments: clarify ecommerce application tests comments

--- src/test/java/com/codelegends/ecommerce/ECommerceApplicationTests.java
+++ src/test/java/com/codelegends/ecommerce/ECommerceApplicationTests.java
@@ -1,7 +1,9 @@
 package com.codelegends.ecommerce;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+// Defines the ECommerceApplicationTests type used by the ecommerce app
 @SpringBootTest class ECommerceApplicationTests {
+    // Run this check as a Spring Boot test
     @Test void contextLoads() {
     }
 }
