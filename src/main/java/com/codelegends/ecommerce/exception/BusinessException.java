4f83b023fbdf3304846c110b05f3b86c6e7dd45a
MOHAMMED ALSHANDOODI <mohed.said77@gmail.com>
2026-09-06 14:55:48 +0400

    comments: clarify business exception inheritance

--- src/main/java/com/codelegends/ecommerce/exception/BusinessException.java
+++ src/main/java/com/codelegends/ecommerce/exception/BusinessException.java
@@ -1,6 +1,7 @@
 package com.codelegends.ecommerce.exception;
 // Represents an error case that the API can report clearly
 // Represents a business rule problem in the app
+// Extend RuntimeException so callers are not forced to catch it
 public class BusinessException extends RuntimeException {
     public BusinessException(String message) {
         super(message);
